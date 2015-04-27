20806109aa37193dbdfdacc0776453f03b835c45
jarmoTest <jarmoTest@martti>
2015-04-27 14:34:00 +0300

    Fixed updates

--- validate.php
+++ validate.php
@@ -28,28 +28,38 @@
 		
 	
 	*/
+
 	if(strlen($username) == 0 && strlen($password) > 0) {
 		
 		// Connecting to db, http://www.w3schools.com/php/php_mysql_connect.asp
 		// PRACTISE! 	Get the database connection to work! What to do?
-		//				Tip: Ask about connection credentials from instructor
+		//				Tip: Connection credentials:
+		//				hostname: localhost
+		//				username: testuser
+		//				password: kissa123
+		//				database: testday_db
 		/*
 			Answer:
 			
 		*/
 		
-		$connection = mysqli_connect("host", "username", "password", "database") or die("Error connecting to database, ".mysqli_error($connection))
+		
+			
+		$connection = mysqli_connect("", "", "", "") or die("Error connecting to database; ".mysqli_error($connection));
 		
 		// Selecting from database: http://www.w3schools.com/php/php_mysql_select.asp
 		// Practise! 	Make the query for getting username, 
 		// "Give me all data from 'users' table, where the username field is like $username"
 		
 		//Answer to the QUERY HERE
-		//Btw, check what "mysql_real_escape_string()" does ( "Google PHP mysql_real_escape_string")
-		$query = mysql_real_escape_string("QUERY HERE = ".$username.";") or die("Error in query, ".mysqli_error($connection));
+		//Btw, check what "mysqli_real_escape_string()" does ( "Google PHP mysqli_real_escape_string")
+				
+		$username = mysqli_real_escape_string($connection, $username);
+
+		$query = "QUERY HERE = '".$username."';";
 		
 		//Executing phase shift.... Not. Executing query.
-		$result = $connection->query($query);
+		$result = $connection->query($query) or die("Error in query execution: ".mysqli_error($connection));
 		
 		$user = null;
 		
@@ -64,16 +74,21 @@
 			//Make sure that the username + password combination is ok!
 			// The password is hashed in the database using sha1 http://www.w3schools.com/php/func_string_sha1.asp
 			//	PRACTISE! hash the user given password, and compare it to the password from the database
-			//	( Aka. fill in the if() )
+			//	( Aka. replace the if(1==1) with right if(rules_here) )
 			//	Also, just to be sure, compare the usernames also
-			if() {
-				$user = $singleUser['name']
+			
+			if(1==1) {
+				$user = $singleUser['name'];
 				break;
 			}
+			else
+			{
+				echo "<h2 style='color: red;'>Wrong password!</h2>";
+			}
 		}
-		// 	Make it that if the user variable is not null, 
+		// 	Make it that if the user variable is not null,  ( replace if(1==1) )
 		//	aka the user is validated and saved to $user, print the "Welcome"
-		if()
+		if(1==1)
 			echo "<h2>Welcome, ".$user."!</h2><p>Congrats! Practises over! Get a coffee or something</p>";
 		else
 			echo "<p>Username or password mismatch, <span style='color: red'>FAIL</span><p>";
